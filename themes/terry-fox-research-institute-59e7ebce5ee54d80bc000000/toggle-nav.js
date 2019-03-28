d4d155f7886daef6328e866907b65b3825175ba2
Joseph Masongsong <joseph@josephmasongsong.com>
2019-03-27 20:10:15 -0700

    small adjustments to nav, and mobile (ipad) styling

--- themes/terry-fox-research-institute-59e7ebce5ee54d80bc000000/toggle-nav.js
+++ themes/terry-fox-research-institute-59e7ebce5ee54d80bc000000/toggle-nav.js
@@ -1,10 +1,10 @@
 $(window).scroll(function() {
 if ($(document).scrollTop() >= 50) {
-  $('.navbar-brand img').attr('src', 'https://d3n8a8pro7vhmx.cloudfront.net/themes/59e7ebce5ee54d80bc000000/attachments/original/1538025314/marathonofhope1.png?1538025314');
+  $('.navbar-brand img').attr('src', 'https://d3n8a8pro7vhmx.cloudfront.net/themes/59e7ebce5ee54d80bc000000/attachments/original/1553277453/rsz_marathonofhope-brightred-01.png?1553277453');
 } else if ($(document).scrollTop() <= 50) {
   $('.navbar-brand img').attr('src', 'https://d3n8a8pro7vhmx.cloudfront.net/themes/59e7ebce5ee54d80bc000000/attachments/original/1538025314/marathonofhope_whitetext1.png?1538025314');
 }
-});      
+});
 $(window).scroll(function() {
 if ($(document).scrollTop() > 50) {
   $('.main-nav').addClass('shrink');
